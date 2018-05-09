a5ef29691dd081bd656f755c2117492c53e4466f
EduardoArthur <38742213+EduardoArthur@users.noreply.github.com>
2018-05-08 23:37:43 -0300

    Update vetor.c

--- vetor.c
+++ vetor.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int main(void){
-	char t;
+	int t;
 	char temps[50];
 	int x;
 	int cont=5;
 	int i;
 	int s=0;
-	for (i=1, i<=cont ,i++){
-		printf("escreva a temperatura:\n")
-                scanf("%c", &t)
-		temps[i]=t
-		s=s+temps[i]
+	for (i=1;i<=cont;i++){
+		printf("escreva a temperatura:\n");
+                scanf("%d", &t);
+		temps[i]=t;
+		s=s+temps[i];
 		}
-	x=s/cont
-	printf("media:", x)
-	s=0
-	for (i=1, i<=cont, i++){
+	x=s/cont;
+	printf("media:%d\n", x);
+	s=0;
+	for (i=1;i<=cont;i++){
 		if (x>temps[i]){
-			s=s+1
+			s=s+1;
+			}
 		}
-	printf("temperaturas acima da media:",s)
-	return 0
+	printf("temperaturas acima da media:%d\n",s);
+	return 0;
 	}
-		
