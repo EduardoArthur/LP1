9055b915198e317448e0b85d6fde5a0aa5a40802
EduardoArthur <38742213+EduardoArthur@users.noreply.github.com>
2018-06-18 19:43:17 -0300

    Update 7.3.c

--- exercicios dos slides/7.3.c
+++ exercicios dos slides/7.3.c
@@ -1,9 +1,44 @@
-#include <stdio.h>
-#define L 3
-#define C 2
-int mat[L][C] = {{0,1}, {3,0}, {0,0}};
-int main(void){
-	int i,j;
+#include"stdio.h"
+#define L 10
+#define C 10
+struct Posicao{
+	int x;
+	int y;
+};
+struct personagem{
+	int id;
+	struct Posicao pos;
+	int pts;
+};
+void leia_posicao(struct Posicao *pos){
+	printf("x: ");
+	scanf("%d", &(*pos).x);
+	printf("y: ");
+	scanf("%d", &(*pos).y);
+}
+void leia_personagem(struct personagem *p){
+	printf("ID do personagem:\n");
+	scanf("%d", &(*p).id);
+	leia_posicao(&(*p).pos);
+	(*p).pts = 0;
+}
+
+int main (void){
+	struct personagem v[10];
+	int i;
+	int j;
+	int mat[L][C]={{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0}};
+
+	for (i=0;i<10;i++){
+		leia_personagem(&v[i]);
+		}
+	for (i=0;i<10;i++){
+	printf("ID: %d\n", v[i].id);
+        printf("Posição: x=%d y=%d\n", v[i].pos.x, v[i].pos.y);
+	mat[v[i].pos.x][v[i].pos.y]=v[i].id;
+	}
+	//cria matriz
+			
 	printf("  ");
 	for (j=0; j<C; j++){
 		printf("%d ",j);
@@ -21,5 +56,6 @@ int main(void){
 		}
 		printf("\n");
 	}
+	
 	return 0;
 }
