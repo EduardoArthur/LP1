bb51422853a3d0f13a5d69429a2a6cdd430ffea9
EduardoArthur <38742213+EduardoArthur@users.noreply.github.com>
2018-07-15 22:27:36 -0300

    Update 8.4.c

--- exercicios dos slides/8.4.c
+++ exercicios dos slides/8.4.c
@@ -3,21 +3,21 @@
 
 void escrevastring(FILE* f, char* str){
 	int j;
-	f=fopen("/tmp/arq-02.txt", "w");
 	for(j=0;j<strlen(str);j++){
-			fputc(str[j], f);
-		}
-	fclose(f);
+		fputc(str[j], f);
+	}
 }
 
 int main(void){
 	char v[10][25];
 	int i;
 	FILE* f;
+	f=fopen("/tmp/arq-02.txt", "w");
 	for(i=0;i<10;i++){
 		printf("digite a string %d:", i+1);
 		scanf("%s", v[i]);
 		escrevastring(f,v[i]);
 	}
+	fclose(f);
 	return 0;	
 }
